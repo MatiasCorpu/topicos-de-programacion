43ea16dbc0041f24902197626ede9204ff54445f
Matias Corpu <164563483+MatiasCorpu@users.noreply.github.com>
2024-09-11 14:30:08 -0300

    modified static_libraries/array/array.h

--- static_libraries/array/array.h
+++ static_libraries/array/array.h
@@ -19,12 +19,12 @@ void showInt(void *x);
 void shiftLeftOverwrite(void *current, void *last, size_t sizeElem);
 void shiftRightWithInsert(void *current, void *last, size_t sizeElem);
 
-void arrayRemoveAllOccurrences(void *arr, int *ce, size_t sizeElem, void *elem, Compare cmp);
-void arrayRemoveFirstOccurrence(void *arr, int *ce, size_t sizeElem, void *elem, Compare cmp);
-void arrayRemoveElementAtPosition(void *arr, int *ce, size_t sizeElem, int pos);
+void arrayPrint(void *arr, int ce, size_t sizeElem, Show show);
+
 void arrayInsertElementAtPosition(void *arr, int *ce, size_t sizeElem, int cap, void *elem, int pos);
 int arrayInsertInSortedOrder(void *arr, int *ce, size_t sizeElem, int cap, void *elem, Compare cmp);
-
-void arrayPrint(void *arr, int ce, size_t sizeElem, Show show);
+void arrayRemoveElementAtPosition(void *arr, int *ce, size_t sizeElem, int pos);
+void arrayRemoveFirstOccurrence(void *arr, int *ce, size_t sizeElem, void *elem, Compare cmp);
+void arrayRemoveAllOccurrences(void *arr, int *ce, size_t sizeElem, void *elem, Compare cmp);
 
 #endif // ARRAY_H_INCLUDED
